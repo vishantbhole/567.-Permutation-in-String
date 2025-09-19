d5a810c711d796f3584bf9e4e9eaa6399a9c9e54
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-18 20:04:04 -0500

    Update checkInclusion.py

--- checkInclusion.py
+++ checkInclusion.py
@@ -17,3 +17,13 @@ class Solution(object):
         matches = 0
         for i in range(26):
             matches += (1 if s1Count[i] == s2Count[i] else 0)
+         
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count [index] + 1 == s2Count[index]:
+                matches -= 1
