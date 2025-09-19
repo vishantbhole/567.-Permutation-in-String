fc2bd5a4f08fefcd09d7b8b021bb44a8995471b3
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-18 20:03:23 -0500

    Update checkInclusion.py

--- checkInclusion.py
+++ checkInclusion.py
@@ -10,3 +10,10 @@ class Solution(object):
         if len(s1) > len(s2): return False
 
         s1Count, s2Count = [0] * 26, [0] * 26
+        
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
